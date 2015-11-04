98fc50547fb2ae74e5fe9a9c1043b3f050abb635
sergey-nevar <serega.nevar@gmail.com>
2015-11-04 22:04:11 +0400

    add constructor to TableView

--- src/sample/TaskTable.java
+++ src/sample/TaskTable.java
@@ -6,14 +6,25 @@ import javafx.scene.control.TableView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class TaskTable {
-	private List taskList;
+	private List<Task> taskList;
 	private TableView tableView;
 	private Button addButton;
 	private Button deleteButton;
 	private Label tableName;
 	private HBox buttonLayout;
 	private VBox mainLayout;
+
+	TaskTable(){
+		tableView = new TableView();
+		taskList = new ArrayList<>();
+		addButton = new Button("Add");
+		deleteButton = new Button("Delete");
+		tableName = new Label("Some table");
+		buttonLayout = new HBox(addButton, deleteButton);
+		mainLayout = new VBox(tableName, tableView, buttonLayout);
+	}
 }
