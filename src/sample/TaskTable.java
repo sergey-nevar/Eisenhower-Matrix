ff97a6ecdc05590a873456ef62c7316e9f09c7c7
sergey-nevar <serega.nevar@gmail.com>
2015-11-04 22:31:36 +0400

    add alignment in TaskTabel

--- src/sample/TaskTable.java
+++ src/sample/TaskTable.java
@@ -1,11 +1,14 @@
 package sample;
 
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,16 +20,23 @@ public class TaskTable {
 	private Button deleteButton;
 	private Label tableName;
 	private HBox buttonLayout;
+	private VBox textLayout;
 	private VBox mainLayout;
 
 	TaskTable(){
 		tableView = new TableView();
 		taskList = new ArrayList<>();
+
+		tableName = new Label("Some table");
+		textLayout = new VBox(tableName);
+		textLayout.setAlignment(Pos.CENTER);
+
 		addButton = new Button("Add");
 		deleteButton = new Button("Delete");
-		tableName = new Label("Some table");
 		buttonLayout = new HBox(addButton, deleteButton);
-		mainLayout = new VBox(tableName, tableView, buttonLayout);
+		buttonLayout.setAlignment(Pos.CENTER);
+
+		mainLayout = new VBox(textLayout, tableView, buttonLayout);
 	}
 
 	VBox getMainLayout(){
