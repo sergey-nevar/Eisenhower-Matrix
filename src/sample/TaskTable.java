735f06bb550f45f78cff42ff4a1af0789a25b054
sergey-nevar <serega.nevar@gmail.com>
2015-11-04 20:25:56 +0400

    create class TaskTable

---
+++ src/sample/TaskTable.java
@@ -0,0 +1,14 @@
+package sample;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import java.util.List;
+
+public class TaskTable {
+	private List taskList;
+	private TableView tableView;
+	private Button addButton;
+	private Button deleteButton;
+	private Label tableName;
+}
