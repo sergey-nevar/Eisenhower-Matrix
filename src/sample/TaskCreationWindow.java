4a671b0674c8fefe288a32adf626b352a43a3b04
sergey-nevar <serega.nevar@gmail.com>
2015-11-05 15:38:06 +0400

    add datePicker in TaskCreationWindow

--- src/sample/TaskCreationWindow.java
+++ src/sample/TaskCreationWindow.java
@@ -5,6 +5,8 @@ import javafx.collections.ObservableList;
 import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
 
+import java.time.LocalDate;
+
 public class TaskCreationWindow{
 	private Dialog<Task> creationDialog;
 	TaskCreationWindow(){
@@ -14,17 +16,25 @@ public class TaskCreationWindow{
 
 		Label nameLabel = new Label("Name of task: ");
 		Label priorityLabel = new Label("Priority: ");
+		Label dateLabel = new Label("Deadline date");
 		TextField nameTextField = new TextField();
 
 		ObservableList<String> country = FXCollections.observableArrayList("-", "1", "2", "3", "4", "5");
 		ChoiceBox<String> choiceBox = new ChoiceBox<String>(country);
 
+		DatePicker datePicker = new DatePicker();
+		datePicker.setOnAction(event -> {
+			LocalDate date = datePicker.getValue();
+			System.out.println("Selected date: " + date);
+		});
+
 		GridPane grid = new GridPane();
 		grid.add(nameLabel, 1, 1);
 		grid.add(nameTextField, 2, 1);
-		grid.add(priorityLabel, 1, 2);
-		grid.add(choiceBox, 2, 2);
-
+		grid.add(dateLabel, 1, 2);
+		grid.add(datePicker, 2, 2);
+		grid.add(priorityLabel, 1, 3);
+		grid.add(choiceBox, 2, 3);
 		creationDialog.getDialogPane().setContent(grid);
 
 		ButtonType buttonTypeOk = new ButtonType("Okay", ButtonBar.ButtonData.OK_DONE);
