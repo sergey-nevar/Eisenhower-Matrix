f3139f87cf9de2a0692a77912a116e72cfd9c51a
sergey-nevar <serega.nevar@gmail.com>
2015-11-07 23:33:48 +0400

    add getters in Task

--- src/sample/Task.java
+++ src/sample/Task.java
@@ -10,30 +10,59 @@ import java.util.Date;
 public class Task {
 	private SimpleStringProperty name;
 	private SimpleStringProperty creationTime;
-	private  SimpleStringProperty creationDate;
+	private SimpleStringProperty creationDate;
 	private SimpleStringProperty termTime;
 	private SimpleStringProperty termDate;
-	private SimpleIntegerProperty priority;
+	private SimpleStringProperty priority;
 
-	Task(String name, String termTime, String termDate, int priority){
+	Task(String name, String termTime, String termDate, String priority){
 		this.name = new SimpleStringProperty(name);
 		Calendar calendar = Calendar.getInstance();
 		String currentHour = String.valueOf(calendar.get(Calendar.HOUR)),
-				currentMinute = String.valueOf(calendar.get(Calendar.MINUTE)),
-				currentSecond = String.valueOf(calendar.get(Calendar.SECOND));
-		creationTime = new SimpleStringProperty(currentHour + ':' + currentMinute + ':' + currentSecond);
+				currentMinute = String.valueOf(calendar.get(Calendar.MINUTE));
+		creationTime = new SimpleStringProperty(currentHour + ':' + currentMinute);
 		String currentYear = String.valueOf(calendar.get(Calendar.YEAR)),
 				currentMonth = String.valueOf(calendar.get(Calendar.MONTH)),
 				currentDate = String.valueOf(calendar.get(Calendar.DATE));
-		creationDate = new SimpleStringProperty(currentDate + '.' + currentMonth + '.' + currentYear);
+		creationDate = new SimpleStringProperty(currentYear + '-' + currentMonth + '-' + currentDate);
 		this.termTime = new SimpleStringProperty(termTime);
 		this.termDate = new SimpleStringProperty(termDate);
-		this.priority = new SimpleIntegerProperty(priority);
+		this.priority = new SimpleStringProperty(priority);
 	}
 	public StringProperty nameProperty(){
 		return name;
 	}
 	public String getName(){
 		return name.getValue();
+	}
+	public StringProperty termTimeProperty(){
+		return termTime;
+	}
+	public String getTermTime(){
+		return termTime.getName();
+	}
+	public StringProperty termDateProperty(){
+		return termDate;
+	}
+	public String getTermDate(){
+		return termDate.getName();
+	}
+	public StringProperty creationTimeProperty(){
+		return creationTime;
+	}
+	public String getCreationTime(){
+		return creationTime.getName();
+	}
+	public StringProperty creationDateProperty(){
+		return creationDate;
+	}
+	public String getCreationDate(){
+		return creationDate.getName();
+	}
+	public StringProperty priorityProperty(){
+		return priority;
+	}
+	public String getPriotrity(){
+		return priority.getName();
 	}
 }
