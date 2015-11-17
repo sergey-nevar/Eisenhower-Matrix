7f6678a2fedd33e46bc6afffbd4b3062674b1949
sergey-nevar <serega.nevar@gmail.com>
2015-11-17 13:41:55 +0400

    add timerTask and stopChecking in TimerTask

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,9 +11,13 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         primaryStage.setTitle("EisenhowerMatrix");
+        TaskChecker tc = new TaskChecker();
         TaskTable table = new TaskTable("Tasks");
         primaryStage.setScene(new Scene(table.getMainLayout(), 700, 400));
         primaryStage.show();
+        primaryStage.setOnCloseRequest(event -> {
+            tc.stopChecking();
+        });
     }
 
     public static void main(String[] args) {
--- src/sample/TaskChecker.java
+++ src/sample/TaskChecker.java
@@ -1,11 +1,22 @@
 package sample;
 
 import java.util.Timer;
+import java.util.TimerTask;
 
 public class TaskChecker {
 	private Timer taskChecker;
 
 	TaskChecker(){
 		taskChecker = new Timer();
+		taskChecker.schedule(new TimerTask() {
+			@Override
+			public void run() {
+				System.out.println("12345");
+			}
+		}, 0, 10000);
+	}
+
+	public void stopChecking(){
+		taskChecker.cancel();
 	}
 }
