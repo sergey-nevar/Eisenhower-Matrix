bfffc567832bbcb97d1f44372954cf130a141ab3
sergey-nevar <serega.nevar@gmail.com>
2015-11-17 21:39:15 +0400

    add getting table names in MainWindow

--- src/sample/MainWindow.java
+++ src/sample/MainWindow.java
@@ -2,14 +2,20 @@ package sample;
 
 import javafx.scene.layout.GridPane;
 
+import java.util.ArrayList;
+
 public class MainWindow {
 	private GridPane gridPane;
+	private static final String tableName1 = "A";
+	private static final String tableName2 = "B";
+	private static final String tableName3 = "C";
+	private static final String tableName4 = "D";
 
 	MainWindow(){
-		TaskTable tableA = new TaskTable("A");
-		TaskTable tableB = new TaskTable("B");
-		TaskTable tableC = new TaskTable("C");
-		TaskTable tableD = new TaskTable("D");
+		TaskTable tableA = new TaskTable(tableName1);
+		TaskTable tableB = new TaskTable(tableName2);
+		TaskTable tableC = new TaskTable(tableName3);
+		TaskTable tableD = new TaskTable(tableName4);
 
 		gridPane = new GridPane();
 		gridPane.add(tableA.getMainLayout(), 0, 0);
@@ -18,6 +24,14 @@ public class MainWindow {
 		gridPane.add(tableD.getMainLayout(), 1, 1);
 	}
 
+	public static ArrayList<String> getTableNames(){
+		ArrayList<String> resulList = new ArrayList<String>();
+		resulList.add(tableName1);
+		resulList.add(tableName2);
+		resulList.add(tableName3);
+		resulList.add(tableName4);
+		return resulList;
+	}
 	public GridPane getMainLayout(){
 		return gridPane;
 	}
